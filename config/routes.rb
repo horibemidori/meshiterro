7fcf48595f92d7130ab97cbf7c742d961f4be984
EC2 Default User <ec2-user@ip-172-31-42-58.ap-northeast-1.compute.internal>
2024-08-13 09:32:11 +0000

    管理者機能追加１

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :admin, skip: [:registrations, :password], controllers: {sessions: 'admin/sessions' }
+  namespace :admin do
+    get 'dashboards', to:'dashboards#index'
+    resource :users, only: [:destroy]
+  end
   devise_for :users
   root to: "homes#top"
 
